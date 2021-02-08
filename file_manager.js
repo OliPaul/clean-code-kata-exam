52e8480f4be06f358cbce880427a33760c908b47
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-02-09 00:31:32 +0100

    Adding deletion in file

--- file_manager.js
+++ file_manager.js
@@ -1,18 +1,42 @@
-const fs =  require('fs'); 
+const fs = require('fs');
 
 module.exports = {
-    write: function(file, data){
-        const isWrote = false
-        fs.appendFile(file, data + "\n", function(error){
-            if(!error){
+    write: function (file, data) {
+        var isWrote = false
+        fs.appendFile(file, data + "\n", function (error) {
+            if (!error) {
                 isWrote = true;
             }
         });
 
         return isWrote;
     },
-    read: function(file){
+    read: function (file) {
         const data = fs.readFileSync(file, "utf8");
         return data;
+    },
+    delete: function (file, dataToDelete) {
+        var isDeleted = false;
+
+        isDeleted = fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                isDeleted = false;
+                return;
+            }
+
+            fs.writeFile(file, dataToDelete, 'utf8', function (err) {
+                if (err) {
+                    console.log(err);
+                    isDeleted = false;
+                    return;
+                }
+                isDeleted = true;
+            });
+
+            return isDeleted;
+        });
+
+        return isDeleted;
     }
 }
