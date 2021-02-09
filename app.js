df28138cda6e2b85b22e2942175f39cb34216cf4
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-02-09 01:47:20 +0100

    Adding entry point to test this app

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const library = require("./library/library");
+const user = require("./user/user");
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,7 +8,110 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Kata for clean code');
+
+  var url = req.url;
+
+  /**
+   * This function allow Librarian to add a new book
+   * Example of object :
+   * {
+   *     "userID": 1,
+   *     "role": 1,
+   *     "book": {
+   *         "name": "Alibaba et les 40 voleurs",
+   *         "author": "Françoise Christophe"
+   *     }
+   *   }
+   */
+  if (url === '/new-book') {
+    var body = "";
+    req.on("data", buffer => {
+      body += buffer.toString();
+    })
+
+    req.on("end", () => {
+      const data = JSON.parse(body);
+      //Autentificate user
+      const oUser = user.auth(data.userID, data.role);
+      //Perform action
+      const message = library.newBook(data.book.name, data.book.author, oUser);
+      res.write(message);
+      res.end();
+    })
+
+  } 
+  /**
+   * This function allow Client to see book list
+   */
+  else if (url === '/book-list') {
+    //Perform action
+    const message = library.displayAllBooks();
+    res.write(JSON.stringify(message));
+    res.end();
+  } 
+
+  /**
+   * This function allow Member to borrow a book list
+   * Example of object :
+   * {
+   *     "userID": 1,
+   *     "role": 2,
+   *     "book": [{
+   *         "bookName": "Alibaba et les 40 voleurs",
+   *         "authorName": "Françoise Christophe"
+   *     }]
+   *   }
+   */
+  else if (url === '/borrow-book') {
+    var body = "";
+    req.on("data", buffer => {
+      body += buffer.toString();
+    })
+
+    req.on("end", () => {
+      const data = JSON.parse(body);
+      //Autentificate user
+      const oUser = user.auth(data.userID, data.role);
+      //Perform action
+      const message = library.borrowBook(data.book, oUser);
+      res.write(message);
+      res.end();
+    })
+  } 
+
+  /**
+   * This function allow Member to return a book list
+   * Example of object :
+   * {
+   *     "userID": 1,
+   *     "role": 2,
+   *     "book": [{
+   *         "bookName": "Alibaba et les 40 voleurs",
+   *         "authorName": "Françoise Christophe"
+   *     }]
+   *   }
+   */
+  else if (url === '/return-book') {
+    var body = "";
+    req.on("data", buffer => {
+      body += buffer.toString();
+    })
+
+    req.on("end", () => {
+      const data = JSON.parse(body);
+      //Autentificate user
+      const oUser = user.auth(data.userID, data.role);
+      //Perform action
+      const message = library.returnBook(data.book, oUser);
+      res.write(message);
+      res.end();
+    })
+  } 
+  
+  else {
+    res.write('Clean Code Kata');
+    res.end();
+  }
 });
 
 server.listen(port, hostname, () => {
